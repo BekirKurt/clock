fc208f0a5decc86a9dd77bf94cabb54d891063c0
Bekir Kurt <73036927+BekirKurt@users.noreply.github.com>
2022-04-10 00:02:30 +0300

    Update script.js

--- analogclock/script.js
+++ analogclock/script.js
@@ -7,11 +7,11 @@ function myFunction() {
   var sn = date.getSeconds();
   var dk = date.getMinutes();
 
-  var yelkovan = document.getElementById("saniye");
+  var sec = document.getElementById("saniye");
   var akrep = document.getElementById("akrep");
   var dakika = document.getElementById("yelkovan");
 
-  yelkovan.style.transform = "rotate(" + (sn*6) + "deg)";
+  sec.style.transform = "rotate(" + (sn*6) + "deg)";
   dakika.style.transform = "rotate(" + dk * 6 + "deg)";
   akrep.style.transform = "rotate("+ (saat +(dk/60))*30  +"deg)";
 }
